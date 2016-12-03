3406e05367c16508f158a586ab1f2b15527029b5
abhinay.pandey@raspsolutions.com <Abhi@006>
2016-12-03 21:35:06 +0530

    added routes

--- index.js
+++ index.js
@@ -1,20 +1,82 @@
-var express = require('express');
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts_collection";
+
 var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 5000));
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+var db_uri= "mongodb://abhinay@mongodb:abhinay@ds119788.mlab.com:19788/abhidb_contacts ";
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(uri, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-app.use(express.static(__dirname + '/public'));
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 5000, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// CONTACTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+app.get("/contacts", function(req, res) {
 });
 
+app.post("/contacts", function(req, res) { 
+  var newContact = req.body;
+  newContact.createDate = new Date();
 
+  if (!(req.body.firstName || req.body.lastName)) {
+    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+  }
+
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+
+
+});
+
+/*  "/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/contacts/:id", function(req, res) {
+});
+
+app.put("/contacts/:id", function(req, res) {
+});
+
+app.delete("/contacts/:id", function(req, res) {
+});
